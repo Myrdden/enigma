9b2f733368e2cb4e14a991b0e5ed5a8d524ceae1
Aurie <Myrdden42@gmail.com>
2019-03-13 16:28:53 -0600

    Add notches and step function

--- rotor.rb
+++ rotor.rb
@@ -1,8 +1,11 @@
 class Rotor
-    def initialize(key)
+    def initialize(key, date)
         @key = key
         @setting = spool(("a".."z").to_a, 0, [])
-        puts @setting
+        tmp = (date**2).to_s
+        @notches = [tmp[-2..-1], tmp[-4..-3], tmp[-6..-5], tmp[-8..-7]]
+        @current = 0
+        require 'pry';binding.pry
     end
     def spool(inp, i, out)
         if inp.empty?
@@ -13,6 +16,12 @@ class Rotor
             return spool(inp, i, out)
         end
     end
+    def step
+        forward = @notches.any? {|x| @setting[@current] == x} ? true : false
+        @current += 1
+        @current = 0 if @current > 25
+        return forward
+    end
 end
 
-test = Rotor.new(5)
+test = Rotor.new(5, 101010)
