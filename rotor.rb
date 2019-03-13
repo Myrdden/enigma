8c1954710f3dc0ffd4d3138373214fbf49e1a62a
Aurie <Myrdden42@gmail.com>
2019-03-13 16:55:46 -0600

    Add char routing functions

--- rotor.rb
+++ rotor.rb
@@ -1,12 +1,11 @@
 class Rotor
     def initialize(key, date)
         @key = key
-        abcs = ("a".."z").to_a
+        abcs = ("A".."Z").to_a
         tmp = (date**2).to_s
-        @notches = [abcs[tmp[-2..-1].to_i], abcs[tmp[-4..-3].to_i], abcs[tmp[-6..-5].to_i], abcs[tmp[-8..-7].to_i]]
+        @notches = [abcs[tmp[-2..-1].to_i], abcs[tmp[-4..-3].to_i], abcs[tmp[-6..-5].to_i], abcs[tmp[-8..-7].to_i]].uniq
         @setting = spool(abcs, 0, [])
         @current = 0
-        require 'pry';binding.pry
     end
     def spool(inp, i, out)
         if inp.empty?
@@ -23,6 +22,17 @@ class Rotor
         @current = 0 if @current > 25
         return forward
     end
+    def reset
+        @current = 0
+    end
+    def forward(inp)
+        return @setting[(inp.ord - 65)]
+    end
+    def reverse(inp)
+        return (@setting.index(inp) + 65).chr
+    end
 end
 
-test = Rotor.new(5, 101010)
+test = Rotor.new(5, 123456)
+puts test.forward("C")
+puts test.reverse(test.forward("C"))
