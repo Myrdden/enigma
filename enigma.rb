0277b4afba1737669547985e28c61d1a49ad9b47
Aurie <Myrdden42@gmail.com>
2019-03-14 13:44:24 -0600

    Add enigma and plugboard

---
+++ enigma.rb
@@ -0,0 +1,11 @@
+require_relative 'rotor.rb'
+
+def setup
+    
+end
+
+def encrypt(msg)
+    msg.each_char do |x|
+
+    end
+end
--- rotor.rb
+++ rotor.rb
@@ -3,7 +3,7 @@ class Rotor
         @key = key
         abcs = ("A".."Z").to_a
         tmp = (date**2).to_s
-        @notches = [abcs[tmp[-2..-1].to_i], abcs[tmp[-4..-3].to_i], abcs[tmp[-6..-5].to_i], abcs[tmp[-8..-7].to_i]].uniq
+        @notches = [abcs[tmp[-2..-1].to_i % 26], abcs[tmp[-4..-3].to_i % 26], abcs[tmp[-6..-5].to_i % 26], abcs[tmp[-8..-7].to_i % 26]].uniq
         @setting = spool(abcs, 0, [])
         @current = 0
     end
@@ -32,7 +32,3 @@ class Rotor
         return (@setting.index(inp) + 65).chr
     end
 end
-
-test = Rotor.new(5, 123456)
-puts test.forward("C")
-puts test.reverse(test.forward("C"))
