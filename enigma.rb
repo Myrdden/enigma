5860e167ccce5f6442c5ad255d4138353288a5c1
Aurie <Myrdden42@gmail.com>
2019-03-14 20:25:00 -0600

    Begun enigma init method

--- enigma.rb
+++ enigma.rb
@@ -1,12 +1,14 @@
 require_relative 'rotor.rb'
 
 class Enigma
-    def initialize(numRotors = nil, key = nil) #:: Int, Int
-        @key = key | Random.rand(("1" + ("0" * ((numRotors + 1) * 4 - 1))).to_i..("9" * ((numRotors + 1) * 4)).to_i)
+    attr_reader :key
+    def initialize(numRotors = nil, key = nil) #:: Int, String
+        numRotors = numRotors | (key ? (key.size / 4) : 3)
+        @key = key | Random.rand(("1" + ("0" * ((numRotors + 1) * 4 - 1))).to_i..("9" * ((numRotors + 1) * 4)).to_i).to_s
         numRotors.times do |i|
-
+            @rotors << Rotor.new(@key[()], @key[].to_i)
         end
-        @reflector = Rotor.new()
+        @reflector = Rotor.new(@key[-4..-3], @key[-2..-1])
     end
 
     def encrypt(msg)
